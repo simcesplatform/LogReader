d67e6e499569dbc7e6e746fc860bed670af324b3
Otto Hylli <otto.hylli@tuni.fi>
2020-07-07 11:11:10 +0300

    Use suffix with /simulations route

--- LogReader/controllers/simulations.py
+++ LogReader/controllers/simulations.py
@@ -26,7 +26,7 @@ class SimController:
         self._simulationStore = simulationStore
         
     @falcon.before( LogReader.utils.processDateParams )
-    def on_get( self, req, resp, toDate = None, fromDate = None  ):
+    def on_get_simulations( self, req, resp, toDate = None, fromDate = None  ):
         '''
         Process request for getting list of simulations.
         The LogReader.utils.processDateParams is used as a falcon before hook to convert fromDate and toDate query parameters to datetime objects.
