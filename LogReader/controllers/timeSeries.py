c878cb39ebc68398666ddf45ed88fabfc8fe7d99
Otto Hylli <otto.hylli@tuni.fi>
2020-09-07 18:13:30 +0300

    Fix simple time series api no longer gives an internal server error when format query param is missing

--- LogReader/controllers/timeSeries.py
+++ LogReader/controllers/timeSeries.py
@@ -27,13 +27,14 @@ class TimeSeriesController():
     def on_get(self, req, resp, simId ):
         log.debug( f'Get time series for simulation with id {simId} with parameters {req.params}.' )
         params = utils.validateMessageParams( req.params )
-        format = req.get_param( 'format' )
-        if format != None:
-            if format not in [ 'json', 'csv' ]:
+        dataFormat = req.get_param( 'format' )
+        if dataFormat != None:
+            del params['format']
+            if dataFormat not in [ 'json', 'csv' ]:
                 raise falcon.HTTPBadRequest( title = 'Invalid time series result format.', description = f'Format should be json or csv instead of {format}.' )
             
         else:
-            format = 'json'
+            dataFormat = 'json'
             
         attrs = req.get_param( 'attrs', required = True ).split( ',' )
         msgFilter = timeSeries.TimeSeriesMessageFilter( attrs, topic = params.get( 'topic' ), process = params.get( 'process' ))
@@ -41,12 +42,12 @@ class TimeSeriesController():
         del params['process']
         del params['topic']
         del params['onlyWarnings']
-        del params['format']
-        result = timeSeries.getTimeSeries( self._messageStore, simId, [ msgFilter ], csv = format == 'csv', **params )
-        if format == 'json':
+        
+        result = timeSeries.getTimeSeries( self._messageStore, simId, [ msgFilter ], csv = dataFormat == 'csv', **params )
+        if dataFormat == 'json':
             resp.media = result
             
-        elif format == 'csv':
+        elif dataFormat == 'csv':
             resp.content_type = 'text/csv'
             resp.body = result 
         
