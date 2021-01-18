4007f41509c59c6427ae3a11bd9a7f5cfcb21e5b
Ville Heikkilä <ville.heikkila@tuni.fi>
2021-01-18 11:28:22 +0200

    Added logging output for each connection attempt to the HTTP server - using Translogger from paste library - updated pymongo version to be inline with other platform components

--- LogReader/app.py
+++ LogReader/app.py
@@ -2,7 +2,7 @@
 '''
 The main file which sets up the falcon framework based web application.
 If this file  is executed as a main file, the application is started with the waitress WSGI server.
-The WSGI application is available in LogReader.app.api 
+The WSGI application is available in LogReader.app.api
 '''
 
 import os
@@ -11,6 +11,7 @@ import json
 import logging
 
 import falcon
+from paste.translogger import TransLogger
 import waitress
 
 from LogReader.controllers.simulations import SimController
@@ -21,17 +22,17 @@ from LogReader.db import simulations, messages
 from LogReader import utils
 import LogReader
 
-# log for this module 
+# log for this module
 log = logging.getLogger( __name__ )
 
 # configure falcon to automatically convert datetime objects in to JSON when they are a part of the response
 # falcon uses the default python json.dumps method which we will still use but with some parameters preset
-# namely we give our own function for processing objects (datetime) that the default implementation cannot process   
+# namely we give our own function for processing objects (datetime) that the default implementation cannot process
 jsonHandler = falcon.media.JSONHandler(
     dumps = partial(
         json.dumps, default = utils.jsonSerializeDate,
         ensure_ascii=False, sort_keys=True
-        ) 
+        )
     )
 
 extra_handlers = {
@@ -42,7 +43,7 @@ api = falcon.API()
 api.resp_options.media_handlers.update(extra_handlers)
 
 # Add route for getting simulations
-# simController is given the module used to get simulations from the db 
+# simController is given the module used to get simulations from the db
 simController = SimController( simulations )
 api.add_route( '/simulations', simController, suffix = 'simulations' )
 # add route for getting simulation by id.
@@ -54,7 +55,7 @@ api.add_route( '/simulations/{simId}/messages', msgController, suffix = 'message
 api.add_route( '/simulations/{simId}/messages/invalid', msgController, suffix = 'invalid_messages' )
 timeSeriesController = TimeSeriesController( messages )
 api.add_route( '/simulations/{simId}/timeseries', timeSeriesController )
-# add route for the user interface consisting of static files 
+# add route for the user interface consisting of static files
 staticPrefix = '/' # ui available from the root path
 static = StaticSite( staticPrefix ) # use the static controller for delivering the files.
 # note works only on one level e.g. /foo/bar.html does not work.
@@ -66,5 +67,10 @@ if __name__ == '__main__':
     host = os.environ.get( 'LOGREADER_HOST', '*' )
     port = os.environ.get( 'LOGREADER_PORT', 8080 )
     log.info( f'Starting LogReader. Listening on {host}:{port}.' )
-    
-    waitress.serve( api, host = host, port = port )
+
+    # start the waitress server using Translogger to get logging information about each connection attempt
+    waitress.serve(
+        app=TransLogger(api),
+        host=host,
+        port=port
+    )
