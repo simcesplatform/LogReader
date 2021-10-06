c670f942886f14bebf5a5572f0c8a85eb02873eb
Otto Hylli <otto.hylli@tuni.fi>
2021-10-06 16:36:45 +0300

    Add license and copyright information
    - add license file
    - add copyright and license comment to each file

--- LogReader/db/messages.py
+++ LogReader/db/messages.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+# Copyright 2021 Tampere University and VTT Technical Research Centre of Finland
+# This software was developed as a part of the ProCemPlus project: https://www.senecc.fi/projects/procemplus
+# This source code is licensed under the MIT license. See LICENSE in the repository root directory.
+# Author(s): Otto Hylli <otto.hylli@tuni.fi> and Ville Heikkilä <ville.heikkila@tuni.fi>
 '''
 Methods for getting messages and invalid messages from the database.
 '''
--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
+/* Copyright 2021 Tampere University and VTT Technical Research Centre of Finland
+This software was developed as a part of the ProCemPlus project: https://www.senecc.fi/projects/procemplus
+This source code is licensed under the MIT license. See LICENSE in the repository root directory.
+Author(s): Otto Hylli <otto.hylli@tuni.fi> and Ville Heikkilä <ville.heikkila@tuni.fi> */
 function updateLatestQuery(query) {
     document.getElementById("latest_query").innerHTML  = query;
 }
